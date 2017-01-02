545c75ffce5763c3df12be087c3e2dc935372ded
jsnunes <jesiel.nunes@hotmail.com>
2017-01-02 15:29:24 -0200

    Pegando a hora do sitema

---
+++ Assets/DataEHora.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DataEHora : MonoBehaviour {
+	public DateTime _inicio;
+	void Start () { _inicio = DateTime.Now; print (_inicio);}
+	void Update () {}
+}
---
+++ Assets/DataEHora.js
@@ -0,0 +1,4 @@
+﻿#pragma strict
+public var _inicio : Date;
+function Start () {_inicio = new Date().Now; print(_inicio);}
+function Update () {}

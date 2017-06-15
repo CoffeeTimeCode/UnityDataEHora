9d8487df2398e3637db3e559a8b9bfd2ad16ea08
jsnunes <jesiel.nunes@hotmail.com>
2017-06-15 13:31:59 -0300

    Algumas Funções

--- Assets/DataEHora.cs
+++ Assets/DataEHora.cs
@@ -4,6 +4,44 @@ using System;
 
 public class DataEHora : MonoBehaviour {
 	public DateTime _inicio;
-	void Start () { _inicio = DateTime.Now; print (_inicio);}
+
+	void Start () { 
+		_inicio = DateTime.Now; print (_inicio);
+
+		//adicionar dias
+		print (DateTime.Now.AddDays(2));
+
+		//adicionar meses
+		print (DateTime.Now.AddMonths(1));
+
+		//adicionar anos
+		print (DateTime.Now.AddYears(1));
+
+		//adicionar horas
+		print (DateTime.Now.AddHours(2));
+
+		//adicionar minutos
+		print (DateTime.Now.AddMinutes(1));
+
+		//Pegar o dia
+		print (DateTime.Now.Day);
+
+		//Pegar o mês
+		print (DateTime.Now.Month);
+
+		//Pegar o ano
+		print (DateTime.Now.Year);
+
+		//Pegar a hora
+		print (DateTime.Now.Hour);
+
+		//Pegar os minutos
+		print (DateTime.Now.Minute);
+
+		//Pegar os segundos
+		print (DateTime.Now.Second);
+
+	}
+
 	void Update () {}
 }
--- Assets/DataEHora.js
+++ Assets/DataEHora.js
@@ -1,4 +1,39 @@
 ﻿#pragma strict
 public var _inicio : Date;
-function Start () {_inicio = new Date().Now; print(_inicio);}
+function Start () {
+	_inicio = new Date().Now; print(_inicio);
+	
+	//adicionar dias
+	print (new Date().Now.AddDays(2));
+
+	//adicionar meses
+	print (new Date().Now.AddMonths(1));
+
+	//adicionar anos
+	print (new Date().Now.AddYears(1));
+
+	//adicionar horas
+	print (new Date().Now.AddHours(2));
+
+	//adicionar minutos
+	print (new Date().Now.AddMinutes(1));
+
+	//Pegar o dia
+	print (new Date().Now.Day);
+
+	//Pegar o mês
+	print (new Date().Now.Month);
+
+	//Pegar o ano
+	print (new Date().Now.Year);
+
+	//Pegar a hora
+	print (new Date().Now.Hour);
+
+	//Pegar os minutos
+	print (new Date().Now.Minute);
+
+	//Pegar os segundos
+	print (new Date().Now.Second);
+}
 function Update () {}
